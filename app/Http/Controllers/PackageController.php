d6d368323022d067ad5ddec6fb2d19198215623e
gededrm <114561837+gede-drm@users.noreply.github.com>
2023-11-03 12:58:51 +0700

    Sec API - Entry Package

--- app/Http/Controllers/PackageController.php
+++ app/Http/Controllers/PackageController.php
@@ -19,10 +19,9 @@ class PackageController extends Controller
             foreach ($packages as $package) {
                 $package['unit_no'] = $package->unit->unit_no;
             }
-            $arrResponse = ['status'=>'success', 'data'=>$packages];
-        }
-        else{
-            $arrResponse = ['status'=>'empty'];
+            $arrResponse = ['status' => 'success', 'data' => $packages];
+        } else {
+            $arrResponse = ['status' => 'empty'];
         }
         return $arrResponse;
     }
@@ -32,28 +31,43 @@ class PackageController extends Controller
     }
     public function secPackageEntry(Request $request)
     {
-        // Belum Selesai
         $date = date('Y-m-d H:i:s');
         $description = $request->get('description');
         $officer_id = $request->get('officer');
         $unit_id = $request->get('unit');
+        $base64Image = $request->get('image');
 
         $incomingPackage = new IncomingPackage();
         $incomingPackage->receive_date = $date;
         $incomingPackage->description = $description;
-        $incomingPackage->photo_url = "";
 
         $unitNo = Unit::select('unit_no')->where('id', $unit_id)->first()->unit_no;
+
+        $img = str_replace('data:image/jpeg;base64,', '', $base64Image);
+        $img = str_replace(' ', '+', $img);
+        $imgData = base64_decode($img);
+        $strCode = $officer_id . $unitNo . $date . 'package';
+        $imgFileName = 'img-qr-' . $unitNo . md5($strCode) . '.png';
+        $imgFileDirectory = '../public/packages/photos/' . $imgFileName;
+        file_put_contents($imgFileDirectory, $imgData);
+
+        $incomingPackage->photo_url = $imgFileName;
+
         $strCode = $officer_id . $unitNo . $date . 'package';
         $verificationCode = hash('sha256', $strCode);
         $qrFileName = 'package-qr-' . $unitNo . md5($strCode) . '.png';
-        $qrFileDirectory = '../public/packages-qr/' . $qrFileName;
+        $qrFileDirectory = '../public/packages/qr-code/' . $qrFileName;
         QrCode::size(500)->margin(2)->color(0, 33, 71)->format('png')->generate($verificationCode, $qrFileDirectory);
 
         $incomingPackage->verification_code = $verificationCode;
         $incomingPackage->qr_url = $qrFileName;
         $incomingPackage->unit_id = 1;
         $incomingPackage->receiving_security_officer_id = 1;
+        $incomingPackage->save();
+
+        $arrResponse = ['status' => 'success'];
+
+        return $arrResponse;
     }
     public function secPackageCollection(Request $request)
     {
