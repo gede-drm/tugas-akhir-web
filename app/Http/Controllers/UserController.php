e30863e9fad3c0a68f682f4f32b4561ef3c4988b
gededrm <114561837+gede-drm@users.noreply.github.com>
2023-11-07 22:58:21 +0700

    SecAPI - Fixed Login & Checkshift Issue

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -20,7 +20,7 @@ class UserController extends Controller
         if ($userSecurity != null) {
             if ($userSecurity->role == 'security') {
                 if (Hash::check($password, $userSecurity->password)) {
-                    $shift = SecurityOfficerCheckin::whereRaw("(check_in like '%".date('Y-m-d')."%' or timestampdiff(minute, now(), check_out)>0)")->orderBy('check_in', 'desc')->where('security_officer_id', $userSecurity->security->id)->first();
+                    $shift = SecurityOfficerCheckin::whereRaw("(timestampdiff(minute, now(), check_in) < 0 and timestampdiff(minute, now(), check_out)>0)")->orderBy('check_in', 'desc')->where('security_officer_id', $userSecurity->security->id)->first();
                     if ($shift != null) {
                         $arrResponse = ['status' => 'success', 'data' => ['security_id' => $userSecurity->security->id, 'security_name' => $userSecurity->security->name, 'username' => $username, 'tower_id' => $shift->tower->id, 'tower_name' => $shift->tower->name]];
                     } else {
@@ -43,12 +43,12 @@ class UserController extends Controller
         $username = $request->get('username');
         $tower = $request->get('tower');
         $userSecurity = User::select('id')->where('username', $username)->first();
-        $shift = SecurityOfficerCheckin::whereRaw("(check_in like '%".date('Y-m-d')."%' or timestampdiff(minute, now(), check_out)>0)")->whereRelation('tower', 'tower_id', $tower)->orderBy('check_in', 'desc')->where('security_officer_id', $userSecurity->security->id)->first();
+        $shift = SecurityOfficerCheckin::whereRaw("(timestampdiff(minute, now(), check_in) < 0 and timestampdiff(minute, now(), check_out)>0)")->whereRelation('tower', 'tower_id', $tower)->orderBy('check_in', 'desc')->where('security_officer_id', $userSecurity->security->id)->first();
         $arrResponse = [];
         if ($shift != null) {
             $arrResponse = ['status' => 'exist'];
         } else {
-            $otherShift = SecurityOfficerCheckin::whereRaw("(check_in like '%".date('Y-m-d')."%' or timestampdiff(minute, now(), check_out)>0)")->orderBy('check_in', 'desc')->where('security_officer_id', $userSecurity->security->id)->first();
+            $otherShift = SecurityOfficerCheckin::whereRaw("(timestampdiff(minute, now(), check_in) < 0 and timestampdiff(minute, now(), check_out)>0)")->orderBy('check_in', 'desc')->where('security_officer_id', $userSecurity->security->id)->first();
             if ($otherShift != null) {
                 $arrResponse = ['status' => 'othershift'];
             } else {
