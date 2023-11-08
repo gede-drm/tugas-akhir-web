95bcabebd8445364e0f9c153ca3ce134b443bff5
gededrm <114561837+gede-drm@users.noreply.github.com>
2023-11-08 20:15:40 +0700

    SecAPI - Revisi Entry Package

--- app/Http/Controllers/PackageController.php
+++ app/Http/Controllers/PackageController.php
@@ -10,12 +10,14 @@ use SimpleSoftwareIO\QrCode\Facades\QrCode;
 class PackageController extends Controller
 {
     // Web
-    public function index(){
+    public function index()
+    {
         $incomingPkgs = IncomingPackage::select('id', 'receive_date', 'description', 'photo_url', 'pickup_date', 'unit_id', 'receiving_security_officer_id', 'pickup_security_officer_id')->orderBy('receive_date', 'desc')->get();
         return view('package.index', compact('incomingPkgs'));
     }
-    
-    public function modalPhoto(Request $request){
+
+    public function modalPhoto(Request $request)
+    {
         $package_id = $request->get('package_id');
         $package = IncomingPackage::select('id', 'receive_date', 'description', 'photo_url', 'pickup_date', 'unit_id', 'receiving_security_officer_id', 'pickup_security_officer_id')->where('id', $package_id)->first();
 
@@ -31,7 +33,7 @@ class PackageController extends Controller
         if (count($packages) > 0) {
             foreach ($packages as $package) {
                 $package['unit_no'] = $package->unit->unit_no;
-                $package->photo_url = "https://gede-darma.my.id/packages/photos/".$package->photo_url;
+                $package->photo_url = "https://gede-darma.my.id/packages/photos/" . $package->photo_url;
             }
             $arrResponse = ['status' => 'success', 'data' => $packages];
         } else {
@@ -48,38 +50,50 @@ class PackageController extends Controller
         $date = date('Y-m-d H:i:s');
         $description = $request->get('description');
         $officer_id = $request->get('officer');
-        $unit_id = $request->get('unit');
+        $tower_id = $request->get('tower');
+        $unit_no = $request->get('unit');
         $base64Image = $request->get('image');
 
-        $incomingPackage = new IncomingPackage();
-        $incomingPackage->receive_date = $date;
-        $incomingPackage->description = $description;
+        $unitId = Unit::select('id')->where('unit_no', $unit_no)->where('tower_id', $tower_id)->first();
 
-        $unitNo = Unit::select('unit_no')->where('id', $unit_id)->first()->unit_no;
+        if ($unitId != null) {
+            $unitId = $unitId->id;
+            $incomingPackage = new IncomingPackage();
+            $incomingPackage->receive_date = $date;
+            $incomingPackage->description = $description;
 
-        $img = str_replace('data:image/jpeg;base64,', '', $base64Image);
-        $img = str_replace(' ', '+', $img);
-        $imgData = base64_decode($img);
-        $strCode = $officer_id . $unitNo . $date . 'package';
-        $imgFileName = 'img-package-' . $unitNo . md5($strCode) . '.png';
-        $imgFileDirectory = '../public/packages/photos/' . $imgFileName;
-        file_put_contents($imgFileDirectory, $imgData);
+            $img = str_replace('data:image/jpeg;base64,', '', $base64Image);
+            $img = str_replace(' ', '+', $img);
+            $imgData = base64_decode($img);
+            $strCode = $officer_id . $unit_no . $date . 'package';
+            $imgFileName = 'img-package-' . $unit_no . md5($strCode) . '.png';
+            $imgFileDirectory = '../public/packages/photos/' . $imgFileName;
+            file_put_contents($imgFileDirectory, $imgData);
 
-        $incomingPackage->photo_url = $imgFileName;
+            $incomingPackage->photo_url = $imgFileName;
 
-        $strCode = $officer_id . $unitNo . $date . 'package';
-        $verificationCode = hash('sha256', $strCode);
-        $qrFileName = 'package-qr-' . $unitNo . md5($strCode) . '.png';
-        $qrFileDirectory = '../public/packages/qr-code/' . $qrFileName;
-        QrCode::size(500)->margin(2)->color(0, 33, 71)->format('png')->generate($verificationCode, $qrFileDirectory);
+            $strCode = $officer_id . $unit_no . $date . 'package';
+            $verificationCode = hash('sha256', $strCode);
+            $qrFileName = 'package-qr-' . $unit_no . md5($strCode) . '.png';
+            $qrFileDirectory = '../public/packages/qr-code/' . $qrFileName;
+            QrCode::size(500)->margin(2)->color(0, 33, 71)->format('png')->generate($verificationCode, $qrFileDirectory);
 
-        $incomingPackage->verification_code = $verificationCode;
-        $incomingPackage->qr_url = $qrFileName;
-        $incomingPackage->unit_id = 1;
-        $incomingPackage->receiving_security_officer_id = 1;
-        $incomingPackage->save();
+            $incomingPackage->verification_code = $verificationCode;
+            $incomingPackage->qr_url = $qrFileName;
+            $incomingPackage->unit_id = $unitId;
+            $incomingPackage->receiving_security_officer_id = $officer_id;
+            $incomingPackage->save();
 
-        $arrResponse = ['status' => 'success'];
+            $arrResponse = ['status' => 'success'];
+        } else {
+            $unitAtOtherTower = Unit::select('id')->where('unit_no', $unit_no)->first();
+            if($unitAtOtherTower != null){
+                $arrResponse = ['status' => 'othertower'];
+            }
+            else{
+                $arrResponse = ['status' => 'notfound'];
+            }
+        }
 
         return $arrResponse;
     }
