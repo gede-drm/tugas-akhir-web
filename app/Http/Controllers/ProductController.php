b3197740b7fad958c4d8d4124fbaafe7aff4ae83
gededrm <114561837+gede-drm@users.noreply.github.com>
2023-11-13 22:39:01 +0700

    TenAPI - Add Product

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Helper;
 use App\Models\Product;
+use App\Models\Tenant;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
@@ -19,25 +20,60 @@ class ProductController extends Controller
         $arrResponse = [];
         if ($tokenValidation == true) {
             $products = Product::select('id', 'name', 'photo_url', 'price', 'stock', 'rating')->where('active_status', 1)->where('tenant_id', $tenant_id)->get();
-            if(count($products)>0){
-                foreach($products as $pro){
-                    $pro->photo_url = "https://gede-darma.my.id/tenants/products/".$pro->photo_url;
+            if (count($products) > 0) {
+                foreach ($products as $pro) {
+                    $pro->photo_url = "https://gede-darma.my.id/tenants/products/" . $pro->photo_url;
                 }
-                $arrResponse = ["status" => "success", "data"=>$products];
-            }
-            else{
+                $arrResponse = ["status" => "success", "data" => $products];
+            } else {
                 $arrResponse = ["status" => "empty"];
             }
-        }
-        else{
+        } else {
             $arrResponse = ["status" => "notauthenticated"];
         }
         return $arrResponse;
-        // Keluarkan Stoknya
     }
     public function tenAddProduct(Request $request)
     {
-    }
+        $tenant_id = $request->get('tenant_id');
+        $name = $request->get('name');
+        $description = $request->get('description');
+        $price = $request->get('price');
+        $base64Image = $request->get('image');
 
-    // Detail nanti catat jumlah terjual berapa
+        $token = $request->get('token');
+        $tokenValidation = Helper::validateToken($token);
+
+        $arrResponse = [];
+        if ($tokenValidation == true) {
+            $tenantName = Tenant::select('name')->where('id', $tenant_id)->first();
+            if ($tenantName != null) {
+                $product = new Product();
+                $product->name = $name;
+                $product->description = $description;
+                $product->price = $price;
+
+                $img = str_replace('data:image/jpeg;base64,', '', $base64Image);
+                $img = str_replace(' ', '+', $img);
+                $imgData = base64_decode($img);
+                $name = str_replace(' ', '-', $name);
+                $tenantName = str_replace(' ', '-', $tenantName->name);
+                $imgFileName = 'img-product-' . $tenantName . '-' . $name . strtotime('now') . '.png';
+                $imgFileDirectory = '../public/tenants/products/' . $imgFileName;
+                file_put_contents($imgFileDirectory, $imgData);
+                $product->photo_url = $imgFileName;
+                $product->stock = 0;
+                $product->rating = 0;
+                $product->active_status = 1;
+                $product->tenant_id = $tenant_id;
+                $product->save();
+                $arrResponse = ["status" => "success"];
+            } else {
+                $arrResponse = ["status" => "tenantnotfound"];
+            }
+        } else {
+            $arrResponse = ["status" => "notauthenticated"];
+        }
+        return $arrResponse;
+    }
 }
