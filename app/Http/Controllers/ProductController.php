bb90756de32765aaabee1e8bc7886878e8c8314a
gededrm <114561837+gede-drm@users.noreply.github.com>
2023-11-14 18:22:09 +0700

    TenAPI - Update Sold's Query on List & Detail

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -24,7 +24,7 @@ class ProductController extends Controller
             if (count($products) > 0) {
                 foreach ($products as $pro) {
                     $pro->photo_url = "https://gede-darma.my.id/tenants/products/" . $pro->photo_url;
-                    $sold = DB::select(DB::raw("select sum(ptd.quantity) as 'sold' from product_transaction_detail ptd inner join transactions t on ptd.transaction_id=t.id inner join transaction_statuses ts on ts.transaction_id=t.id where ptd.product_id = 1 and ts.status='done';"))[0]->sold;
+                    $sold = DB::select(DB::raw("select sum(ptd.quantity) as 'sold' from product_transaction_detail ptd inner join transactions t on ptd.transaction_id=t.id inner join transaction_statuses ts on ts.transaction_id=t.id where ptd.product_id = '".$pro->id."' and ts.status='done';"))[0]->sold;
                     if ($sold == null) {
                         $sold = 0;
                     }
@@ -92,7 +92,7 @@ class ProductController extends Controller
         if ($tokenValidation == true) {
             $product = Product::select('id', 'name', 'description', 'photo_url', 'price', 'stock', 'rating')->where('id', $product_id)->first();
             $product->photo_url = "https://gede-darma.my.id/tenants/products/" . $product->photo_url;
-            $sold = DB::select(DB::raw("select sum(ptd.quantity) as 'sold' from product_transaction_detail ptd inner join transactions t on ptd.transaction_id=t.id inner join transaction_statuses ts on ts.transaction_id=t.id where ptd.product_id = 1 and ts.status='done';"))[0]->sold;
+            $sold = DB::select(DB::raw("select sum(ptd.quantity) as 'sold' from product_transaction_detail ptd inner join transactions t on ptd.transaction_id=t.id inner join transaction_statuses ts on ts.transaction_id=t.id where ptd.product_id = '".$product_id."' and ts.status='done';"))[0]->sold;
             if ($sold == null) {
                 $sold = 0;
             }
