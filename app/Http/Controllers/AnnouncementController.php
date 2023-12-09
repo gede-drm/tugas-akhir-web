a3cd53c9b6d070b822758967a6447f7d23fc10c2
gededrm <114561837+gede-drm@users.noreply.github.com>
2023-12-09 17:07:07 +0700

    Try on AnnouncementFCM

--- app/Http/Controllers/AnnouncementController.php
+++ app/Http/Controllers/AnnouncementController.php
@@ -6,17 +6,20 @@ use App\Models\Announcement;
 use App\Models\Helper;
 use App\Models\User;
 use App\Notifications\SendNotification;
+use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class AnnouncementController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         $announcements = Announcement::orderBy('date', 'desc')->get();
         return view('announcement.index', compact('announcements'));
     }
-    public function store(Request $request){
-        $request->validate(["title"=>"required", "description"=>"required"]);
+    public function store(Request $request)
+    {
+        $request->validate(["title" => "required", "description" => "required"]);
 
         $announcement = new Announcement();
         $announcement->date = date("Y-m-d H:i:s");
@@ -26,33 +29,36 @@ class AnnouncementController extends Controller
         $announcement->save();
 
         $userResidents = User::select('id', 'fcm_token')->whereNotNull('fcm_token')->where('role', 'resident')->get();
-        foreach($userResidents as $user){
+        foreach ($userResidents as $user) {
             $title = "Pemberitahuan Baru dari Manajemen";
-            $user->notify(new SendNotification(["title"=>$title, "body"=>$announcement->title]));
+            try {
+                $user->notify(new SendNotification(["title" => $title, "body" => $announcement->title]));
+            } catch (Exception $e) {
+            }
         }
-        
+
         return redirect()->route('announcement.index')->with('status', 'Pemberitahuan Baru Berhasil ditambahkan!');
     }
 
     // API
     // Residents' App API
-    public function rdtGetLatestAnnouncement(Request $request){
+    public function rdtGetLatestAnnouncement(Request $request)
+    {
         $token = $request->get('token');
         $tokenValidation = Helper::validateToken($token);
 
         $arrResponse = [];
         if ($tokenValidation == true) {
             $announcement = Announcement::select('date', 'title', 'description')->orderBy('date', 'desc')->first();
-            if($announcement != null){
+            if ($announcement != null) {
                 $announcement->date = date("d-m-Y H:i", strtotime($announcement->date));
-                $arrResponse = ["status"=>"success", "data"=>$announcement];
-            }
-            else{
-                $arrResponse = ["status" => "empty"];                
+                $arrResponse = ["status" => "success", "data" => $announcement];
+            } else {
+                $arrResponse = ["status" => "empty"];
             }
         } else {
-        $arrResponse = ["status" => "notauthenticated"];
-    }
-    return $arrResponse;
+            $arrResponse = ["status" => "notauthenticated"];
+        }
+        return $arrResponse;
     }
 }
